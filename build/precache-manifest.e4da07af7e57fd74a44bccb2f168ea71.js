a0b38d086bb627df0a634d902199ada22cca1c00
superv2018 <41586952+superv2018@users.noreply.github.com>
2020-07-01 06:46:56 +0300

    added production build

---
+++ build/precache-manifest.e4da07af7e57fd74a44bccb2f168ea71.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04751516504bb9c24169c265ac9357a6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "751ec0ebe5b929108320",
+    "url": "/static/css/main.f73f7164.chunk.css"
+  },
+  {
+    "revision": "56bd7927630a662858c3",
+    "url": "/static/js/2.17e0385b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.17e0385b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "751ec0ebe5b929108320",
+    "url": "/static/js/main.07f1e260.chunk.js"
+  },
+  {
+    "revision": "6be06fbc24171e2fd061",
+    "url": "/static/js/runtime-main.110b1f99.js"
+  }
+]);
