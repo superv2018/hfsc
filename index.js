55ad88c2f0968ba1a0c58fee47fa59e18eb86e5b
superv2018 <41586952+superv2018@users.noreply.github.com>
2020-07-11 00:15:13 +0300

    added validation

--- index.js
+++ index.js
@@ -1,89 +1,133 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const Note = require("./models/note");
+const { update } = require("./models/note");
+
+app.use(cors());
+app.use(express.static("build"));
+app.use(express.json());
 
 
-app.use(express.json())
-app.use(cors())
-app.use(express.static('build'))
 
 let notes = [
-    {
-      id: 1,
-      content: "HTML is easy",
-      date: "2019-05-30T17:30:31.098Z",
-      important: true
-    },
-    {
-      id: 2,
-      content: "Browser can execute only Javascript",
-      date: "2019-05-30T18:39:34.091Z",
-      important: false
-    },
-    {
-      id: 3,
-      content: "GET and POST are the most important methods of HTTP protocol",
-      date: "2019-05-30T19:20:14.298Z",
-      important: true
-    }
-  ]
+  {
+    id: 1,
+    content: "HTML is easy",
+    date: "2019-05-30T17:30:31.098Z",
+    important: true,
+  },
+  {
+    id: 2,
+    content: "Browser can execute only Javascript",
+    date: "2019-05-30T18:39:34.091Z",
+    important: false,
+  },
+  {
+    id: 3,
+    content: "GET and POST are the most important methods of HTTP protocol",
+    date: "2019-05-30T19:20:14.298Z",
+    important: true,
+  },
+];
 
+app.get("/", (req, res) => {
+  res.send("<h1>Hello World, How are you?</h1>");
+});
 
-  app.get('/', (req, res) => {
-      res.send('<h1>Hello World, How are you?</h1>')
-  })
+app.get("/api/notes", (req, res) => {
+  Note.find({}).then((notes) => {
+    res.json(notes);
+  });
+});
 
-  app.get('/api/notes', (req, res) => {
-      res.json(notes)
-  })
-
-  app.get('/api/notes/:id', (req, res) => {
-      const id = Number(req.params.id)
-      const note = notes.find(note => note.id === id)
-
+app.get("/api/notes/:id", (req, res, next) => {
+  Note.findById(req.params.id)
+    .then(note => {
       if (note) {
-          res.json(note)
+        res.json(note);
       } else {
-        res.status(404).end()
+        res.status(404).end();
       }
-  })
+    })
+    .catch(error => next(error))
+});
 
-  app.delete('/api/notes/:id', (req, res) => {
-      const id = Number(req.params.id)
-      notes = notes.filter(note => note.id !== id)
-      res.status(204).end()
-  })
+app.delete("/api/notes/:id", (req, res, next) => {
+ Note.findByIdAndRemove(req.params.id)
+ .then(result => {
+  res.status(204).end();
+ })
+ .catch(error => next(error))
+});
 
+const generateId = () => {
+  const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0;
+  return maxId + 1;
+};
 
-  const generateId = () => {
-    const maxId = notes.length > 0
-        ? Math.max(...notes.map(n => n.id))
-        : 0
-        return maxId + 1
+app.put('/api/notes/:id', (req, res, next) => {
+  const body = req.body
+
+  const note = {
+    content: body.content,
+    important: body.important,
   }
 
-  app.post('/api/notes', (req, res) => {
-      const body = req.body
-      console.log(body, 'hello')
-    
-      if (!body.content) {
-          return res.status(400).json({
-              error: 'content missing'
-          })
-      }
-
-      const note = {
-          content: body.content,
-          important: body.important || false,
-          date: new Date(),
-          id: generateId(),
-      }
-
-      notes = notes.concat(note)
-
-      res.json(note)
+  Note.findByIdAndUpdate(req.params.id, note, {new: true })
+  .then(updateNote => {
+    res.json(updateNote)
   })
-const PORT = process.env.PORT || 3001
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+  .catch(error => next(error))
 })
+
+app.post("/api/notes", (req, res, next) => {
+  const body = req.body;
+  //console.log(body, "hello");
+
+  //if (body.content === undefined) {
+    //return res.status(400).json({
+      //error: "content missing",
+    //});
+  //}
+
+//Save note to mongodb
+  const note = new Note({
+    content: body.content,
+    important: body.important || false,
+    date: new Date(),
+  });
+
+  note.save()
+  .then(savedNote => savedNote.toJSON())
+  .then(savedAndFormattedNote => {
+    res.json(savedAndFormattedNote)
+  })
+  .catch(error => next(error))
+});
+
+const unknownEndpoint = (req, res) => {
+  res.status(404).send({ error: 'unkwown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+const errorHandler = (error, req, res, next) => {
+  console.error(error.message)
+
+  if(error.name === 'CastError') {
+    return res.status(400).send({ error: 'malformatted id'})
+  } else if (error.name === 'ValidationError') {
+    return res.status(400).json({ error: error.message })
+  }
+
+  next(error)
+}
+
+app.use(errorHandler)
+
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
